7d91e93e3618f7bc9ce389278ac584135eafc70f
KasJnr16 <abdulaikassimjr@gmail.com>
2023-12-04 02:31:38 +0000

    found the bug

--- marketPlace/item/forms.py
+++ marketPlace/item/forms.py
@@ -1,30 +1,17 @@
 from django import forms
-from .models import(
-    Item
-)
+from .models import Item
 
 INPUT_CLASSES = "w-full py-4 px-6 rounded-xl border"
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ("category", "name", "description", "price", "image",)
+        fields = ("category", "name", "description", "price", "image")
 
-        # another way to use the widgets
         widgets = {
-            "category" : forms.Select(attrs={
-                "class": INPUT_CLASSES
-            }),
-            "name" : forms.TextInput(attrs={
-                "class": INPUT_CLASSES
-            }),
-            "description" : forms.Textarea(attrs={
-                "class": INPUT_CLASSES
-            }),
-            "price" : forms.TextInput(attrs={
-                "class": INPUT_CLASSES
-            }),
-            "image" : forms.FileInput(attrs={
-                "class": INPUT_CLASSES
-            }),
-
+            "category": forms.Select(attrs={"class": INPUT_CLASSES}),
+            "name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "description": forms.Textarea(attrs={"class": INPUT_CLASSES}),
+            "price": forms.NumberInput(attrs={"class": INPUT_CLASSES}),
+            "image": forms.FileInput(attrs={"class": INPUT_CLASSES}),
         }
